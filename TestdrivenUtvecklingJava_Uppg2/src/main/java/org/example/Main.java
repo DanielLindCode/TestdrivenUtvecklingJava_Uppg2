90b4311809d0f8a37c99c7434b289462f531a39f
undeadnerd <lindbergdaniel1988@gmail.com>
2023-11-15 14:45:36 +0100

    first

---
+++ TestdrivenUtvecklingJava_Uppg2/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+//TODO Create Classes
+//TODO Think of necessary functions I need
+//TODO Test functions
+//TODO Implement them to the code
